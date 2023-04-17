be82cb131ba44bb2e920584d22be23a4d28f72b6
Jheyson Toscano <jeisonbra30@gmail.com>
2023-04-16 22:54:17 -0500

    Continuacion de eliminar y cargar imagenes

--- src/main/java/com/ecommerce/ecommerce/services/uploadFileService.java
+++ src/main/java/com/ecommerce/ecommerce/services/uploadFileService.java
@@ -1,2 +1,42 @@
-package com.ecommerce.ecommerce.services;public class uploadFileService {
+package com.ecommerce.ecommerce.services;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+
+@Service
+public class uploadFileService {
+
+    String folder = "images://";
+
+
+    public String saveImage(MultipartFile file) throws IOException {
+
+        if(!file.isEmpty()){
+
+            byte bytes[] = file.getBytes();
+            Path patch = Paths.get(folder+file.getOriginalFilename());
+            Files.write(patch,bytes);
+            return file.getOriginalFilename();
+        }
+
+
+        return "default.jpg";
+    }
+
+    public void delete(String nombre){
+        String ruta = "images//";
+        File file = new File(ruta+nombre);
+        file.delete();
+    }
+
+
+
 }
